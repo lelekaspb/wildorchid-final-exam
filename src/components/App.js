8d4e4033a385e92823ba33876bb3afa3087ee487
lelekaspb <bov-1986@yandex.ru>
2022-05-13 12:34:47 +0200

    added react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
-import heading from "../../styles/Heading.module.css";
-import text from "../../styles/Text.module.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./Navbar";
+import Services from "./Services";
+import AboutUs from "./AboutUs";
+import LandingPage from "./LandingPage";
+import Contact from "./Contact";
 
 function App() {
+  const [activePage, setActivePage] = useState("home");
+
   return (
     <div className="App">
-      <h1 class={heading.header}>Wild Orchid Website</h1>
-      <p class={text.header}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <BrowserRouter>
+        <Navbar activePage={activePage} setActivePage={setActivePage} />
+
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about_us" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
