cf91799664f6747c186628704b70a4abad880254
Daria <dariastoicad@gmail.com>
2022-05-20 12:25:37 +0200

    resizing text and reposition cvr number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,65 +6,65 @@ import footer from "../styles/Footer.module.css";
 export default function Footer({ activePage }) {
   return (
     <footer>
-      <div className={` ${footer.Category} `}>
-        <h1 className={` ${footer.CategoryName} `}>
-          <FormattedMessage id="footer.hours" defaultMessage="Åbningstider" />
-        </h1>
-        <div className={` ${footer.CategoryTextDiv} `}>
-          <p className={` ${footer.CategoryText} `}>
-            <FormattedMessage
-              id="footer.hours.one"
-              defaultMessage="Tirsdag - Torsdag: 11 - 19"
-            />
-          </p>
-          <p className={` ${footer.CategoryText} `}>
-            <FormattedMessage
-              id="footer.hours.two"
-              defaultMessage="Fredag: 9 - 17"
-            />
-          </p>
-          <p className={` ${footer.CategoryText} `}>
-            <FormattedMessage
-              id="footer.hours.three"
-              defaultMessage="Lørdag: 9 - 16"
-            />
-          </p>
-          <p className={` ${footer.CategoryText} `}>
-            <FormattedMessage
-              id="footer.hours.four"
-              defaultMessage="Søndag - Mandag: Lukket"
-            />
-          </p>
+      <div className={` ${footer.FlexWrap} `}>
+        <div className={` ${footer.Category} `}>
+          <h1 className={` ${footer.CategoryName} `}>
+            <FormattedMessage id="footer.hours" defaultMessage="Åbningstider" />
+          </h1>
+          <div className={` ${footer.CategoryTextDiv} `}>
+            <p className={` ${footer.CategoryText} `}>
+              <FormattedMessage
+                id="footer.hours.one"
+                defaultMessage="Tirsdag - Torsdag: 11 - 19"
+              />
+            </p>
+            <p className={` ${footer.CategoryText} `}>
+              <FormattedMessage
+                id="footer.hours.two"
+                defaultMessage="Fredag: 9 - 17"
+              />
+            </p>
+            <p className={` ${footer.CategoryText} `}>
+              <FormattedMessage
+                id="footer.hours.three"
+                defaultMessage="Lørdag: 9 - 16"
+              />
+            </p>
+            <p className={` ${footer.CategoryText} `}>
+              <FormattedMessage
+                id="footer.hours.four"
+                defaultMessage="Søndag - Mandag: Lukket"
+              />
+            </p>
+          </div>
+        </div>
+
+        <div className={` ${footer.Category} `}>
+          <h1 className={` ${footer.CategoryName} `}>
+            <FormattedMessage id="footer.address" defaultMessage="Adresse" />
+          </h1>
+          <div className={` ${footer.CategoryTextDiv} `}>
+            <p className={` ${footer.CategoryText} `}>Rolighedsvej 19,</p>
+            <p className={` ${footer.CategoryText} `}>1958 Frederiksberg</p>
+            <div className={` ${footer.CategoryContact} `}></div>
+            <p className={` ${footer.CategoryText} `}>info@wildorchid.dk</p>
+            <p className={` ${footer.CategoryText} `}>+45 93 93 81 63</p>
+          </div>
+        </div>
+
+        <div className={` ${footer.Category} `}>
+          <h1 className={` ${footer.CategoryName} `}>
+            <FormattedMessage id="follow" defaultMessage="Følg oss" />
+          </h1>
         </div>
       </div>
 
-      <div className={` ${footer.Category} `}>
-        <h1 className={` ${footer.CategoryName} `}>
-          <FormattedMessage id="footer.address" defaultMessage="Adresse" />
-        </h1>
-        <div className={` ${footer.CategoryTextDiv} `}>
-          <p className={` ${footer.CategoryText} `}>Rolighedsvej 19,</p>
-          <p className={` ${footer.CategoryText} `}>1958 Frederiksberg</p>
-          <div className={` ${footer.CategoryContact} `}></div>
-          <p className={` ${footer.CategoryText} `}>info@wildorchid.dk</p>
-          <p className={` ${footer.CategoryText} `}>+45 93 93 81 63</p>
-        </div>
-      </div>
-
-      <div className={` ${footer.Category} `}>
-        <h1 className={` ${footer.CategoryName} `}>
-          <FormattedMessage id="follow" defaultMessage="Følg oss" />
-        </h1>
-      </div>
-
-      <div className={` ${footer.Category} `}>
-        <p className={` ${footer.CVRnumber} `}>
-          <FormattedMessage
-            id="footer.cvr"
-            defaultMessage="CVR-nummer: 37892726"
-          />
-        </p>
-      </div>
+      <p className={` ${footer.CVRnumber} `}>
+        <FormattedMessage
+          id="footer.cvr"
+          defaultMessage="CVR-nummer: 37892726"
+        />
+      </p>
     </footer>
   );
 }
