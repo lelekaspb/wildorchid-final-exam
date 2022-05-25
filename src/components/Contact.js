e602188f4747c0e3044e2f43b16d4717ec53bd66
Daria <dariastoicad@gmail.com>
2022-05-25 14:43:50 +0200

    removing smth useless

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -166,16 +166,6 @@ function Contact() {
           </div>
         </div>
 
-        {/* <iframe
-          title="wildorchid"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.236726178961!2d12.53938015182533!3d55.68487198043961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465253f591c5993d%3A0x1011c06cef26fe04!2sWild%20Orchid!5e0!3m2!1sen!2sse!4v1653394275961!5m2!1sen!2sse"
-          width="100px"
-          height="450px"
-          style="border:0;"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe> */}
         <div className={` ${contact.MapWrap} ${contact.MobileHidden}  `}>
           <iframe
             className={contact.map}
