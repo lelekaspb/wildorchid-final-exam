74825bc91945e0287e419608dfb585f5e500563a
Daria <dariastoicad@gmail.com>
2022-05-25 22:13:45 +0200

    finally made line break work w/ intl

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,29 +7,34 @@ function Home() {
     <>
       <div className={home.background}>
         <Navbar activePage="home" />
-        <div className={home.WelcomeWrap}>
-          <h2 className={home.Welcomeh2}>
-            <FormattedMessage
-              id="greeting.one"
-              defaultMessage="Velkommen til"
-            />
-          </h2>
-          <h1 className={home.Welcomeh1}>Wild Orchid</h1>
-          <h2 className={home.Welcomeh2}>
-            <FormattedMessage
-              id="greeting.two"
-              defaultMessage="Vi venter på dig i vores udsøgte skønhedssklinik"
-            />
-          </h2>
-        </div>
+        <div className={home.AllWelcomeWrap}>
+          <div className={home.WelcomeWrap}>
+            <h2 className={home.Welcomeh2}>
+              <FormattedMessage
+                id="greeting.one"
+                defaultMessage="Velkommen til"
+              />
+            </h2>
+            <h1 className={home.Welcomeh1}>Wild Orchid</h1>
+            <h2 className={home.Welcomeh2}>
+              <FormattedMessage
+                id="greeting.two"
+                defaultMessage="Vi venter på dig i vores udsøgte skønhedssklinik"
+                values={{
+                  breakingLine: <br />,
+                }}
+              />
+            </h2>
+          </div>
 
-        <div className={home.WelcomePicWrap}>
-          <div className={home.WelcomePicBorder}>
-            <img
-              className={home.WelcomePic}
-              src="./photos/homepage-girl.jpg"
-              alt=""
-            ></img>
+          <div className={home.WelcomePicWrap}>
+            <div className={home.WelcomePicBorder}>
+              <img
+                className={home.WelcomePic}
+                src="./photos/homepage-girl.jpg"
+                alt=""
+              ></img>
+            </div>
           </div>
         </div>
       </div>
