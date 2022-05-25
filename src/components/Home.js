89ab30f54631dc55b519e13dadc47be8fb3077bc
Daria <dariastoicad@gmail.com>
2022-05-25 16:10:34 +0200

    some welcome text an pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,27 @@
 import { FormattedMessage } from "react-intl";
+import home from "./../styles/Homepage.module.css";
 import Navbar from "./Navbar";
 
 function Home() {
   return (
     <>
       <Navbar activePage="home" />
-      <h1>
-        <FormattedMessage id="greeting" defaultMessage="Wild Orchid Webside" />
-      </h1>
+      <div className={home.WelcomeWrap}>
+        <h2 className={home.Welcomeh2}>
+          <FormattedMessage id="greeting.one" defaultMessage="Velkommen til" />
+        </h2>
+        <h1 className={home.Welcomeh1}>Wild Orchid</h1>
+        <h2 className={home.Welcomeh2}>
+          <FormattedMessage
+            id="greeting.two"
+            defaultMessage="Vi venter på dig i vores udsøgte skønhedssklinik"
+          />
+        </h2>
+      </div>
+
+      <div className={home.WelcomePicBorder}>
+        <img className={home.WelcomePic} src="./photos/homepage-girl.jpg"></img>
+      </div>
     </>
   );
 }
