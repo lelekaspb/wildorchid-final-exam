addcfaea0d918392725f44676d9d5b554730eb0d
Daria <dariastoicad@gmail.com>
2022-06-01 21:04:45 +0200

    nomatch

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -6,7 +6,13 @@ function NoMatch() {
   return (
     <div className={nomatch.NoMatchContainer}>
       <div className={nomatch.ErrorWrap}>
-        <p>Sorry, page not found!</p>
+        <p>
+          <FormattedMessage
+            id="nomatch.notfound"
+            defaultMessage="Beklager, siden blev ikke fundet!"
+          />
+          Sorry, page not found!
+        </p>
         <img src="./icons/notfound.svg" alt="Sad face"></img>
       </div>
 
@@ -17,7 +23,7 @@ function NoMatch() {
           alt="Return icon"
         ></img>
         <Link to="/" className={nomatch.BackText}>
-          Return home
+          <FormattedMessage id="nomatch.back" defaultMessage="Vende hjem" />
         </Link>
       </div>
     </div>
